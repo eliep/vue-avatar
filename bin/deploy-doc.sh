2e311b68960834a1e973957cbffcccd790a5ae36
Elie Prudhomme <eprudhomme@v3d.fr>
2016-05-10 22:35:52 +0200

    Update deploy-doc script

--- bin/deploy-doc.sh
+++ bin/deploy-doc.sh
@@ -4,10 +4,12 @@ git stash save 'Before deploy' # Stash all changes before deploy
 git checkout master
 npm run build
 cp dist/vue-avatar.min.js gh-pages/
-# npm run build # Generate the bundled Javascript and CSS
-if $(git commit -am Deploy docs); then # Commit the changes, if any
+git add gh-pages/vue-avatar.min.js
+git commit -m "Deploy docs"
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
   echo 'Changes Committed'
 fi
-#git push heroku deploy:master # Deploy to Heroku
-#git checkout master # Checkout master again
+git subtree push --prefix gh-pages origin gh-pages # Deploy gh-pages
+git push origin master
 git stash pop # And restore the changes
