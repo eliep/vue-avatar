73afe1e4fcaa1e033779e1bf5be829e897371734
Elie Prudhomme <eprudhomme@v3d.fr>
2016-10-07 22:38:38 +0200

    Update for Vue 2.0

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -16,7 +16,8 @@ module.exports = {
     extensions: ['', '.js', '.vue'],
     fallback: [path.join(__dirname, 'node_modules')],
     alias: {
-      'src': path.resolve(__dirname, '../src')
+      'src': path.resolve(__dirname, '../src'),
+      vue: 'vue/dist/vue.js'
     }
   },
   resolveLoader: {
