2829844bc118fb830d9c1b9be92e57e3c68a2e8b
Diego Jara <saintplay96@gmail.com>
2017-11-14 23:10:25 -0500

    update deploy script

--- bin/deploy-doc.sh
+++ bin/deploy-doc.sh
@@ -2,15 +2,13 @@
 
 git stash save 'Before deploy' # Stash all changes before deploy
 git checkout master
-npm run build
-cp dist/vue-avatar.min.js gh-pages/
-git add gh-pages/vue-avatar.min.js
+yarn docs
 git commit -m "Deploy docs"
 RESULT=$?
 if [ $RESULT -eq 0 ]; then
   echo ' >> Changes Committed << '
 fi
-git subtree push --prefix gh-pages origin gh-pages # Deploy gh-pages
+git subtree push --prefix docs origin gh-pages # Deploy gh-pages
 git push origin master
 git stash pop # And restore the changes
 echo ' >> Deployed << '
